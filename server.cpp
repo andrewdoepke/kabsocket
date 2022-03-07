59d8981069d0988a4ac1a1d1462ca40db7782677
Andrew Doepke <andrewdoepke@outlook.com>
2022-03-07 16:36:58 -0600

    init of server cpp file

---
+++ server.cpp
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 6069
+
+int main(int argc, char const *argv[]) {
+
+}
