a7dcaf3d81c195c03169400eeb86a1b07584ef52
Andrew Doepke <andrewdoepke@outlook.com>
2022-03-07 16:45:19 -0600

    server implementation
    Per boost.org tutorial

--- server.cpp
+++ server.cpp
@@ -1,11 +1,38 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#define PORT 6069
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
 
-int main(int argc, char const *argv[]) {
+#DEFINE PORT 6069
 
+using boost::asio::ip::tcp;
+
+
+std::string make_daytime_string() {
+  using namespace std; // For time_t, time and ctime;
+  time_t now = time(0);
+  return ctime(&now);
+}
+
+
+int main() {
+  try {
+	boost::asio::io_context io_context;
+	
+	tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT));
+	
+	for (;;) {
+	  tcp::socket socket(io_context);
+	  acceptor.accept(socket);
+	  
+	  std::string message = make_daytime_string();
+
+	  boost::system::error_code ignored_error;
+	  boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+	}  
+  } catch (std::exception& e) {
+	  std::cerr << e.what() << std::end1;
+  }
+  
+  return 0;
 }
